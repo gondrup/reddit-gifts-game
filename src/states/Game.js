45b1e51272d0e23149ad70b941958905a6375110
Adrian Harding <gondrup@gmail.com>
2017-12-06 21:55:37 +0000

    Got original test working in new framework

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,34 +1,67 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+//import Mushroom from '../sprites/Mushroom'
+
+const ASSET_DIR = 'assets';
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
-
-  create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
-  }
-
-  render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    preload() {
+        this.game.load.image('sky', ASSET_DIR + '/milky_way_stars_night_sky_space_97654_800x600.jpg');
+        this.game.load.spritesheet('dude', ASSET_DIR + '/dude.png', 32, 48);
     }
-  }
+
+    create() {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.game.add.sprite(0, 0, 'sky');
+
+        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+
+        //  We need to enable physics on the player
+        this.game.physics.arcade.enable(this.player);
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        this.player.body.bounce.y = 0.2;
+        this.player.body.gravity.y = 300;
+        this.player.body.collideWorldBounds = true;
+
+        //  Our two animations, walking left and right.
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        //  Reset the player velocity (movement)
+        this.player.body.velocity.x = 0;
+
+        if (this.cursors.left.isDown)
+        {
+            //  Move to the left
+            this.player.body.velocity.x = -150;
+
+            this.player.animations.play('left');
+        }
+        else if (this.cursors.right.isDown)
+        {
+            //  Move to the right
+            this.player.body.velocity.x = 150;
+
+            this.player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            this.player.animations.stop();
+
+            this.player.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (this.cursors.up.isDown /*&& this.player.body.touching.down*/)
+        {
+            this.player.body.velocity.y = -350;
+        }
+    }
 }

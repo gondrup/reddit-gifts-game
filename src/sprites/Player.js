165e59aea52385f6834b8276bd401f7738a6faa0
Adrian Harding <adrian.harding@vcars.co.uk>
2017-12-07 14:22:57 +0000

    Basic game set-up using classes

---
+++ src/sprites/Player.js
@@ -0,0 +1,67 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Sprite {
+    constructor ({ game, x, y, asset, frame }) {
+        super(game, x, y, asset, frame);
+
+        this.game = game;
+
+        this.anchor.setTo(0.5);
+        this.scale.setTo(1);
+
+        //  We need to enable physics on the player
+        this.game.physics.arcade.enable(this);
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        this.body.bounce.y = 0;
+        this.body.gravity.y = 1000;
+        this.body.collideWorldBounds = true;
+
+        //  Our two animations, walking left and right.
+        this.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        this.frame = 5;
+
+        this.spacebarKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    }
+
+    update () {
+        //  Reset the player velocity (movement)
+        this.body.velocity.x = 0;
+
+        if (this.spacebarKey.downDuration(250)/* && this.body.touching.down*/) {
+            this.body.velocity.y = -350;
+        }
+
+        return;
+
+        if (this.cursors.left.isDown)
+        {
+            //  Move to the left
+            this.body.velocity.x = -150;
+
+            this.animations.play('left');
+        }
+        else if (this.cursors.right.isDown)
+        {
+            //  Move to the right
+            this.body.velocity.x = 150;
+
+            this.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            this.animations.stop();
+
+            this.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (this.cursors.up.isDown /*&& this.player.body.touching.down*/)
+        {
+            this.body.velocity.y = -350;
+        }
+    }
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,11 +1,12 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-//import Mushroom from '../sprites/Mushroom'
 
-const ASSET_DIR = 'assets';
+import Player from '../sprites/Player'
 
 export default class extends Phaser.State {
     preload() {
+        const ASSET_DIR = 'assets';
+
         this.game.load.image('sky', ASSET_DIR + '/milky_way_stars_night_sky_space_97654_800x600.jpg');
         this.game.load.spritesheet('dude', ASSET_DIR + '/dude.png', 32, 48);
     }
@@ -15,53 +16,15 @@ export default class extends Phaser.State {
 
         this.game.add.sprite(0, 0, 'sky');
 
-        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
-
-        //  We need to enable physics on the player
-        this.game.physics.arcade.enable(this.player);
-
-        //  Player physics properties. Give the little guy a slight bounce.
-        this.player.body.bounce.y = 0.2;
-        this.player.body.gravity.y = 300;
-        this.player.body.collideWorldBounds = true;
-
-        //  Our two animations, walking left and right.
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.player = new Player({
+            game: this.game,
+            x: this.game.world.width / 2 - 24,
+            y: this.game.world.height - 16,
+            asset: 'dude'
+        });
+        this.game.add.existing(this.player);
     }
 
     update() {
-        //  Reset the player velocity (movement)
-        this.player.body.velocity.x = 0;
-
-        if (this.cursors.left.isDown)
-        {
-            //  Move to the left
-            this.player.body.velocity.x = -150;
-
-            this.player.animations.play('left');
-        }
-        else if (this.cursors.right.isDown)
-        {
-            //  Move to the right
-            this.player.body.velocity.x = 150;
-
-            this.player.animations.play('right');
-        }
-        else
-        {
-            //  Stand still
-            this.player.animations.stop();
-
-            this.player.frame = 4;
-        }
-
-        //  Allow the player to jump if they are touching the ground.
-        if (this.cursors.up.isDown /*&& this.player.body.touching.down*/)
-        {
-            this.player.body.velocity.y = -350;
-        }
     }
 }
